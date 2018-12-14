02767b45052f21e94862a890a5955fd09643a1f8
Josh Runge <joshrunge@gmail.com>
2018-12-13 21:40:57 -0500

    github fixes

--- static.config.js
+++ static.config.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 export default {
+  siteRoot: "https://jrunge.github.io/multiverse-portfolio/",
   paths: {
     dist: 'docs',
     assets: 'docs'
